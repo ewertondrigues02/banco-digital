01662bdccc7c15984797cba45be404d0a3520b86
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2025-01-18 16:18:44 -0300

    banco-digital criado

---
+++ banco-digital/padrao-camadas/src/test/java/br/com/ewerton/padraocamadas/PadraoCamadasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.padraocamadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadraoCamadasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
